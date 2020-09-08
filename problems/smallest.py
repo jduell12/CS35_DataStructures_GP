d1c4fa31cb79f275cdc75efa17ab5ce677926366
Jessica <duellje@oregonstate.edu>
2020-09-08 13:58:55 -0400

    finished day 2 guided

---
+++ problems/smallest.py
@@ -0,0 +1,6 @@
+# Find the Smallest Missing Element from a Sorted Array
+# Given a sorted arraay of distinct non-negative integers, find the smallest missing element in it.
+
+# Example
+# Input: A = [0, 1, 2, 6, 9, 11, 15]
+# Ouput: 3
