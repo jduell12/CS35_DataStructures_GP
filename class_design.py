5c34f60321d4bd14828626a4e631222284147f7e
Jessica <duellje@oregonstate.edu>
2020-09-10 11:56:42 -0400

    set up for day 4

--- class_design.py
+++ class_design.py
@@ -2,33 +2,17 @@
 # lets design a 2d vector class set
 
 # Think about what data the class will hold
-# x, y coord
+# x, y
 
 # Think about what methods it needs
-# init, str
+# init
+# str
 
 # Think about what methods could be optional
-# add, subtract, 
+# add?
+# sub?
 
 # Draw out diagrams of the class model
-"""
- |------------|  
- | x, y       |    <- fields
- |------------|
- |  __str__   |    <- base methods
- | __init__   |
- |------------|
- |    add     |   <- optional
- |    sub     |
- |    mul     |
- |------------|
- 
- V = Vec2(20, 10)
- V2 = Vec(10, 10)
- V.add(V2) ? overload + operator 
- V += V2 
- V3 = V + V2
-"""
 
 # visualize how it will be formed
 
@@ -37,87 +21,100 @@
 class Vec2:
     def __init__(self, x, y):
         self.x = x
-        self.y = y 
-    
+        self.y = y
+
     def __str__(self):
-        return "Vec2(x = %d, y = %d)" % (self.x, self.y)
-    
-    def add(self, other):
-        self.x += other.x 
-        self.y += other.y 
-        
-        
-    #overload + opartor
-    def __add__(self, other):
-        ret = Vec2(self.x, self.y)
-        ret.x = self.x + other.x 
-        ret.y = self.y + other.y
+        return f"Vec2: ({self.x}, {self.y})"
+
+    # def __str__(self):
+    #     return self.__repr__()
+
+    def add(self, fred):
+        self.x += fred.x
+        self.y += fred.y
+        return Vec2(self.x, self.y)
+    # try not to make nonsensical argument / field names
+    def __add__(self, mouse_spartan_fresco_jinx):
+        ret = Vec2(0, 0)
+        ret.x = self.x + mouse_spartan_fresco_jinx.x
+        ret.y = self.y + mouse_spartan_fresco_jinx.y
         return ret
-    
+
+    # def __sub__(self, other):
+    #     print("I am a fish")
+
     def sub(self, other):
         self.x -= other.x
-        self.y -= other.y 
-        
+        self.y -= other.y
+
     def mul(self, other):
         self.x *= other.x
         self.y *= other.y
-        
-    def divide(self, other):
+
+    def div(self, other):
         self.x /= other.x
-        self.y /= other.y 
-        
+        self.y /= other.y
+
     def idiv(self, other):
         self.x //= other.x
-        self.y //= other.y 
+        self.y //= other.y
 
+
+# reflect on the design & think of improvements
 
 v1 = Vec2(20, 10)
 v2 = Vec2(10, 10)
 
-print('--------------------')
+
 print(v1)
 print(v2)
-print('--------------------')
+
 v1.add(v2)
+
 print(v1)
 print(v2)
-print('--------------------')
+
 v1.sub(v2)
 print(v1)
 print(v2)
-print('--------------------')
-v1.mul(v2)
-print(v1)
-print(v2)
-print('--------------------')
-v1.divide(v2)
-print(v1)
-print(v2)
-print('--------------------')
-v3 = v1 + v2 
-print(v1)
-print(v2)
+
+v3 = v1 + v2
+
+
+
+3 - 5
 print(v3)
-print('--------------------')
+print(v1)
+print(v2)
+
+
 
 # 2.
 # Linked Lists to trees
+
+# SLL
+# (value)->(value)->()
+
+# DLL
+# <-(value)-><-(value)-><-(value)-><-(value)-><-(value)->
+
 """
-Singly linked list
-(value) -> (value) ->()
+DLL
+      (value)        (prev)-(value)-(next)
+      /     \
+(left)     (right)
 
-Doubly linked list
-<- (value) <--> (value) <-->()->
 
-Tree from DLL
-        (value)
-        /     \ 
-    (left)    (right)
-    
-BTNode
-init
-add_left
-add_right
+
+SLL
+    (value)-(next)-(next)
+
+    (value)
+          \
+        (right)
+           \
+           (right)
+
 """
 
 class BTNode:
@@ -125,45 +122,30 @@ class BTNode:
         self.value = value
         self.left = left
         self.right = right
-        
+
     def __add_left__(self, value):
         self.left = BTNode(value)
-    
+
     def __add_right__(self, value):
         self.right = BTNode(value)
-        
+
     def insert(self, value):
-        #left case?
-            #check if value < root value
-            #move to the left and check if it's None
-                #if none insert the node
-                #else call insert on root's left node
-        #right case?
-            #value >= root value
-            #move to right and check if it's None
-                #if none insert the node
-                #else call insert on root's right node 
-        pass
-    
-    #returns true if the tree contains the value or false
-    def contains(self, target):
-        #base case
-            #check if root node value is same as target value
-                #return true
-        #left case
-            #check if target < root value
-                #check if there is no child to the left
-                    #return false 
-                #contains(left child, target)
-        #right case 
-            #check if target > root value
-                #check if no child to the right
-                    #return false
-                #contains(right child, target)
-       
-        pass
-        
-bt1 = BTNode(10) #root
+        # problematic solution?
+        if value >= self.value:
+            self.__add_right__(value)
+        elif value < self.value:
+            self.__add_left__(value)
 
 
-print(bt1.right.value, bt1.left.value)
+
+bt1 = BTNode(10)
+# bt2 = BTNode(20)
+# bt34 = BTNode(8)
+# bt7 = BTNode(5)
+
+# bt1.right = bt2
+# bt1.left = bt34
+# bt34.left = bt7
+
+bt1.add_left(8)
+bt1.add_right(20)
